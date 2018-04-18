06c6c9fa358ab3135a2e3e742c742f4b9642e850
Cryptonious <37422140+Cryptonious@users.noreply.github.com>
2018-04-17 17:00:28 -0700

    Add candles, axis' to new chart

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,12 +1,15 @@
 import React from "react";
-// import styles from "./Chart.css";
+import styles from "./Chart.css";
 // import Auxilary from "../../hoc/Auxilary";
 
-import { scaleLinear } from "d3-scale";
-import { max } from 'd3-array';
+import { scaleLinear, scaleTime } from "d3-scale";
+import { max, min } from 'd3-array';
 import { select } from 'd3-selection';
 import { timeParse } from "d3-time-format";
 import { fitDimensions } from "react-stockcharts/lib/helper";
+import { axisBottom, axisRight, axisLeft } from 'd3-axis';
+import { format } from 'd3';
+import { hexToRGBA } from "react-stockcharts/lib/utils";
 
 // import { AirSwap } from '../../services/AirSwap/AirSwap';
 // import { EthereumTokens } from '../../services/Tokens/Tokens';
@@ -22,6 +25,7 @@ class Chart extends React.Component {
         this.maxHeight = props.height;
         this.maxWidth = props.width;
         this.marginBottom = 220;
+        this.bottomOffset = 20;
 
         this.createChart = this.createChart.bind(this);
     }
@@ -34,34 +38,98 @@ class Chart extends React.Component {
         this.createChart()
     }
 
+    yGen(axisType, scale, width, format) {
+        return axisType(scale)
+            .tickSize(width)
+            .tickFormat(d => {
+                var s = format(d);
+                return "\xa0" + s;
+            });
+    }
+
     createChart() {
-        let chartHeight = this.maxHeight - this.marginBottom;
+        console.log(this.props.data);
+
+        let chartHeight = this.maxHeight - this.marginBottom - this.bottomOffset;
+        let formatNumber = format(".5f");
 
         const node = this.node
-        const dataMax = max(this.props.data)
+        const data = this.props.data;
+
+        const dates = data.map(d => new Date(d.date));
+
+        const xScale = scaleTime()
+            .domain([min(dates), max(dates)])
+            .range([0, this.maxWidth]);
+
+        const values = data.map(d => d.high).concat(data.map(d => d.low)).concat(data.map(d => d.open)).concat(data.map(d => d.close));
+
         const yScale = scaleLinear()
-            .domain([0, dataMax])
-            .range([0, chartHeight])
+            .domain([min(values) < 0 ? min(values) : 0, max(values)])
+            .range([chartHeight, 0]);
+
+        //X-Axis
         select(node)
-            .selectAll('rect')
-            .data(this.props.data)
+            .append("g")
+            .attr('font-family', 'Open Sans')
+            .attr("transform", `translate(0, ${chartHeight})`)
+            .call(axisBottom(xScale));
+
+        //Y-Axis
+        // const yAxis = axisRight(yScale)
+        //     .tickSize(this.maxWidth)
+        //     .tickFormat(d => {
+        //         var s = formatNumber(d);
+        //         return "\xa0" + s;
+        //     });
+
+        const y = select(node)
+            .append('g')
+            .attr('font-family', 'open sans')
+            .attr('font-weight', 'bold')
+            .call(this.yGen(axisRight, yScale, this.maxWidth, formatNumber));
+        y.select(".domain").remove();
+        y.selectAll(".tick:not(:first-of-type) line").attr("stroke", hexToRGBA('#000', 0.15)).attr("stroke-dasharray", "2,4");
+        y.selectAll(".tick text").attr("x", 4).attr("dy", -4);
+
+        const y2 = select(node)
+            .append('g')
+            .attr('font-family', 'open sans')
+            .attr('font-weight', 'bold')
+            .attr('transform', `translate(${this.maxWidth}, 0)`)
+            .call(this.yGen(axisLeft, yScale, this.maxWidth, formatNumber));
+        y2.select(".domain").remove();
+        y2.selectAll("line").remove();
+        // y2.selectAll(".tick:not(:first-of-type) line").attr("stroke", hexToRGBA('#000', 0.15)).attr("stroke-dasharray", "2,4");
+        y2.selectAll(".tick text").attr("x", -4).attr("dy", -4);
+
+        const candleGroup = select(node).append('g');
+
+        // Wicks
+        candleGroup
+            .selectAll('line.wick')
+            .data(data)
+            .enter()
+            .append('line')
+            .attr('class', 'wick')
+            .attr('x1', d => xScale(d.date) + ((this.maxWidth / data.length) - 10) / 2)
+            .attr('x2', d => xScale(d.date) + ((this.maxWidth / data.length) - 10) / 2)
+            .attr('y2', d => yScale(d.high))
+            .attr('y1', d => yScale(d.low))
+            .attr('style', d => `stroke:${d.close < d.open ? '#f54748' : '#34f493'};stroke-width:2`);
+
+        //Candles
+        candleGroup
+            .selectAll('rect.candle')
+            .data(data)
             .enter()
             .append('rect')
-
-        select(node)
-            .selectAll('rect')
-            .data(this.props.data)
-            .exit()
-            .remove()
-
-        select(node)
-            .selectAll('rect')
-            .data(this.props.data)
-            .style('fill', '#fe9922')
-            .attr('x', (d, i) => i * 25)
-            .attr('y', d => chartHeight - yScale(d))
-            .attr('height', d => yScale(d))
-            .attr('width', 25)
+            .attr('class', 'candle')
+            .attr('width', (this.maxWidth / data.length) - 10)
+            .attr('height', d => Math.abs(yScale(d.open) - yScale(d.close)))
+            .attr('x', d => xScale(d.date))
+            .attr('y', d => d.close < d.open ? yScale(d.open) : yScale(d.close))
+            .attr('fill', d => d.close < d.open ? '#f54748' : '#34f493')
     }
 
     render() {
