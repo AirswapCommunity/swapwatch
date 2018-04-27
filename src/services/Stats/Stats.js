e4dce43c6c9011bfcb51f0698db17c190a3e1544
DomiDre <homiedomi@gmail.com>
2018-04-27 10:09:34 +0200

    Remove check for WETH in calculating volume

--- src/services/Stats/Stats.js
+++ src/services/Stats/Stats.js
@@ -3,9 +3,9 @@ var getEthVolume = (trades) => {
   let twentyFourHoursAgo = Date.now() / 1000 - 24*60*60;
   for (let trade of trades) {
     if (trade.timestamp > twentyFourHoursAgo) {
-      if(trade.makerSymbol === 'ETH' || trade.makerSymbol === 'WETH') {
+      if(trade.makerSymbol === 'ETH') {
         volume += trade.makerAmount;
-      } else if(trade.takerSymbol === 'ETH' || trade.takerSymbol === 'WETH') {
+      } else if(trade.takerSymbol === 'ETH') {
         volume += trade.takerAmount;
       }
     }
