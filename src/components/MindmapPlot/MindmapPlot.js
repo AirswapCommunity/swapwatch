7026bdb8c4f3f181ce10f437974edf2d88407558
Domi <DomiDre@users.noreply.github.com>
2018-05-13 07:24:44 -0400

    Change decimal to decimals, import ERC20ABI from Tokens.js (#62)

--- src/components/MindmapPlot/MindmapPlot.js
+++ src/components/MindmapPlot/MindmapPlot.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { withStyles } from 'material-ui/styles';
 import cssStyles from './MindmapPlot.css';
 
+import { EthereumTokens } from '../../services/Tokens/Tokens';
 import vis from 'vis';
 import * as Web3 from 'web3';
 // require('vis/dist/vis-network.min.css');
@@ -10,7 +11,6 @@ import * as Web3 from 'web3';
 const styles = theme => ({
 });
 
-const ERC20_abi = [{"constant": true,"inputs": [],"name": "name","outputs": [{"name": "","type": "string"    }  ],  "payable": false,  "type": "function"},{  "constant": false,  "inputs": [    {"name": "_spender","type": "address"    },    {"name": "_value","type": "uint256"    }  ],  "name": "approve",  "outputs": [    {"name": "success","type": "bool"    }  ],  "payable": false,  "type": "function"},{  "constant": true,  "inputs": [],  "name": "totalSupply",  "outputs": [    {"name": "","type": "uint256"    }  ],  "payable": false,  "type": "function"},{  "constant": false,  "inputs": [    {"name": "_from","type": "address"    },    {"name": "_to","type": "address"    },    {"name": "_value","type": "uint256"    }  ],  "name": "transferFrom",  "outputs": [    {"name": "success","type": "bool"    }  ],  "payable": false,  "type": "function"},{  "constant": true,  "inputs": [],  "name": "decimals",  "outputs": [    {"name": "","type": "uint8"    }  ],  "payable": false,  "type": "function"},{  "constant": true,  "inputs": [],  "name": "version",  "outputs": [    {"name": "","type": "string"    }  ],  "payable": false,  "type": "function"},{  "constant": true,  "inputs": [    {"name": "_owner","type": "address"    }  ],  "name": "balanceOf",  "outputs": [    {"name": "balance","type": "uint256"    }  ],  "payable": false,  "type": "function"},{  "constant": true,  "inputs": [],  "name": "symbol",  "outputs": [    {"name": "","type": "string"    }  ],  "payable": false,  "type": "function"},{  "constant": false,  "inputs": [    {"name": "_to","type": "address"    },    {"name": "_value","type": "uint256"    }  ],  "name": "transfer",  "outputs": [    {"name": "success","type": "bool"    }  ],  "payable": false,  "type": "function"},{  "constant": false,  "inputs": [    {"name": "_spender","type": "address"    },    {"name": "_value","type": "uint256"    },    {"name": "_extraData","type": "bytes"    }  ],  "name": "approveAndCall",  "outputs": [    {"name": "success","type": "bool"    }  ],  "payable": false,  "type": "function"},{  "constant": true,  "inputs": [    {"name": "_owner","type": "address"    },    {"name": "_spender","type": "address"    }  ],  "name": "allowance",  "outputs": [    {"name": "remaining","type": "uint256"    }  ],  "payable": false,  "type": "function"},{  "inputs": [    {"name": "_initialAmount","type": "uint256"    },    {"name": "_tokenName","type": "string"    },    {"name": "_decimalUnits","type": "uint8"    },    {"name": "_tokenSymbol","type": "string"    }  ],  "type": "constructor"},{  "payable": false,  "type": "fallback"},{  "anonymous": false,  "inputs": [    {"indexed": true,"name": "_from","type": "address"    },    {"indexed": true,"name": "_to","type": "address"    },    {"indexed": false,"name": "_value","type": "uint256"    }  ],  "name": "Transfer",  "type": "event"},{  "anonymous": false,  "inputs": [    {"indexed": true,"name": "_owner","type": "address"    },    {"indexed": true,"name": "_spender","type": "address"    },    {"indexed": false,"name": "_value","type": "uint256"    }  ],  "name": "Approval",  "type": "event"},]
 const ETH_address = '0x0000000000000000000000000000000000000000';
 
 class MindmapPlot extends Component {
@@ -44,9 +44,9 @@ class MindmapPlot extends Component {
       let contractToken2;
 
       if(token1.address !== ETH_address)
-        contractToken1 = new web3.eth.Contract(ERC20_abi, token1.address);
+        contractToken1 = new web3.eth.Contract(EthereumTokens.ERC20ABI, token1.address);
       if(token2.address !== ETH_address)
-        contractToken2 = new web3.eth.Contract(ERC20_abi, token2.address);
+        contractToken2 = new web3.eth.Contract(EthereumTokens.ERC20ABI, token2.address);
       this.setState({web3: web3,
                      isConnected: connected,
                      contractToken1: contractToken1,
@@ -97,7 +97,7 @@ class MindmapPlot extends Component {
         promisesBalance.push(
           this.state.contractToken1.methods.balanceOf(nodeAddressList[i]).call()
           .then((balance) => {
-            nodeToken1BalanceList[i] = balance / 10**this.props.token1.decimal;
+            nodeToken1BalanceList[i] = balance / 10**this.props.token1.decimals;
           })
         )
       else
@@ -111,7 +111,7 @@ class MindmapPlot extends Component {
         promisesBalance.push(
           this.state.contractToken2.methods.balanceOf(nodeAddressList[i]).call()
           .then((balance) => {
-            nodeToken2BalanceList[i] = balance / 10**this.props.token2.decimal;
+            nodeToken2BalanceList[i] = balance / 10**this.props.token2.decimals;
           })
         )
       else
